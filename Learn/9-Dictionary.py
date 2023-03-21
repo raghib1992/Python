46388de6fa1d372e565d3f9da439293709ea64b3
raghib1992 <raghib.npti@gmail.com>
2023-03-21 12:05:21 +0530

    Add dictionary

--- Learn/9-Dictionary.py
+++ Learn/9-Dictionary.py
@@ -1,15 +1,15 @@
 families = {"Afsa": 2, "Umar": 2, "Innaya": 4}
 
-print(families.keys())
-print(families.values())
-print(families.items())
+# print(families.keys())
+# print(families.values())
+# print(families.items())
 
 for family in families:
     print(family)
-    print(families)
-    print(f"{family} age is {families[family]}")
+    # print(families)
+    # print(f"{family} age is {families[family]}")
 
-for family, age in families.items():
-    print (f"{family} age is {age}")
+# for family, age in families.items():
+#     # print (f"{family} age is {age}")
 
 
--- dictionary.py
+++ dictionary.py
@@ -1,14 +1,15 @@
 family = {"Raghib":29, "Rashid":26, "Shaheen":21}
 
 family["Ilma"]=3
+family["Abraaz"]=1
+# print(family)
+# print(family["Raghib"])
 
-print(family["Raghib"])
-
-for fa in family:
-    print(f"{fa}:{family[fa]}")
-print(fa)
-for fa, age in family.items():
-    print(f"{fa}: {age}")
+# for fa in family:
+# #     print(f"{fa}:{family[fa]}")
+# # print(fa)
+# for fa, age in family.items():
+#     print(f"{fa}: {age}")
 
 
 home = [
@@ -17,15 +18,14 @@ home = [
     {"name": "Shaheen", "age": 21},
 ]
 
-print(home[1])
-
+print(home)
 
 student = {"Ram": 95, "Sita":99, "Goyal":97}
 
-if "Ram" in student:
+if 95 in student.values():
     print(f"Ram: {student['Ram']}")
 else:
-    print("Ram is not the student of the class")
+    print("Jitendra is not the student of the class")
 
 attendance = student.values()
-print(f"The average attendance of the whole class {sum(attendance)/len(attendance)}")
+# print(f"The average attendance of the whole class {sum(attendance)/len(attendance)}")
