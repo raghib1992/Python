d3fbb200037650c7e8fa32afc962cd1a51893d77
Nadim <knrs986@astrazeneca.net>
2024-09-30 11:53:53 +0530

    Add day 21

--- python-bootcamp/21-ping-pong-game/main.py
+++ python-bootcamp/21-ping-pong-game/main.py
@@ -1,6 +1,8 @@
 from turtle import Turtle, Screen
 from paddle import Paddle
-
+from ball import Ball
+import time
+from scoreboard import Scoreboard
 
 # Setup screen
 screen = Screen()
@@ -10,23 +12,45 @@ screen.title('Pong')
 screen.tracer(0)
 
 # Create Paddle
-l_paddle = Paddle(380)
-r_paddle = Paddle(-380)
+r_paddle = Paddle(380)
+l_paddle = Paddle(-380)
 
 # move paddle from input
 screen.listen()
-screen.onkey(l_paddle.go_up, "Up")
-screen.onkey(l_paddle.go_down, "Down")
-screen.onkey(r_paddle.go_up, "w")
-screen.onkey(r_paddle.go_down, "s")
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(r_paddle.go_down, "Down")
+screen.onkey(l_paddle.go_up, "w")
+screen.onkey(l_paddle.go_down, "s")
+
+# Create Ball
+ball = Ball()
+
+r_score = Scoreboard((60,250))
+l_score = Scoreboard((-60,250))
 
 game_on = True
 while game_on:
+    time.sleep(0.07)
     screen.update()
+    ball.move()
+    
+    # Bouncing from top and bottom
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bouncing()
+        
+    # hit r_paddle
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        ball.hit_paddle()
+    
+    #  r_paddle missed
+    if ball.xcor() > 380:
+        ball.reset_ball()
+        l_score.increase_score()
 
- 
-
-
+    #  l_paddle missed
+    if ball.xcor() < -380:
+        ball.reset_ball()
+        r_score.increase_score()
 
 
 screen.exitonclick()
